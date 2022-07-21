a6cfd501bd89d0f64afc79312252be796afc022d
Sruthi Kavuri <kav.sruthi@gmail.com>
2022-07-21 11:28:58 -0700

    final workout screens

---
+++ workoutOptionsScreen/CardioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CardioViewController.swift
+//  workoutOptionsScreen
+//
+//  Created by Sruthi Kavuri on 7/20/22.
+//
+
+import UIKit
+
+class CardioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ workoutOptionsScreen/UpperViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UpperViewController.swift
+//  workoutOptionsScreen
+//
+//  Created by Sruthi Kavuri on 7/20/22.
+//
+
+import UIKit
+
+class UpperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
